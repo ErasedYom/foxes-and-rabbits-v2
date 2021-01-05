0d082fb3945070f23b944e7f9f1abe7945ce4e8a
unknown <m.omar01.m@gmail.com>
2021-01-05 08:31:50 +0000

    First Commit

---
+++ src/Animal.java
@@ -0,0 +1,131 @@
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ * A class representing shared characteristics of animals.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public abstract class Animal
+{
+    // Whether the animal is alive or not.
+    private boolean alive;
+    // The animal's field.
+    private Field field;
+    // The animal's position in the field.
+    private Location location;
+    //
+    protected int age;
+    
+    /**
+     * Create a new animal at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Animal(Field field, Location location)
+    {
+        alive = true;
+        this.field = field;
+        setLocation(location);
+    }
+    
+    /**
+     * Make this animal act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newAnimals A list to receive newly born animals.
+     */
+    abstract public void act(List<Animal> newAnimals);
+
+    /**
+     * Check whether the animal is alive or not.
+     * @return true if the animal is still alive.
+     */
+    protected boolean isAlive()
+    {
+        return alive;
+    }
+
+    /**
+     * Indicate that the animal is no longer alive.
+     * It is removed from the field.
+     */
+    protected void setDead()
+    {
+        alive = false;
+        if(location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+
+    /**
+     * Return the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Place the animal at the new location in the given field.
+     * @param newLocation The animal's new location.
+     */
+    protected void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+
+    public abstract int getBreedingAge();
+
+    public abstract int getMaxAge();
+    public abstract double getBreedingProbability();
+    public abstract int getMaxLitterSize();
+    public abstract Random getRand();
+
+    /**
+     * Return the animal's field.
+     * @return The animal's field.
+     */
+    protected Field getField()
+    {
+        return field;
+    }
+    /**
+     * A fox can breed if it has reached the breeding age.
+     */
+    protected boolean canBreed()
+    {
+        return age >= getBreedingAge();
+    }
+
+    protected void incrementAge()
+    {
+        age++;
+        if(age > getMaxAge()) {
+            setDead();
+        }
+    }
+
+    /**
+     * Generate a number representing the number of births,
+     * if it can breed.
+     * @return The number of births (may be zero).
+     */
+    protected int breed()
+    {
+        int births = 0;
+        if(canBreed() && getRand().nextDouble() <= getBreedingProbability()) {
+            births = getRand().nextInt(getMaxLitterSize()) + 1;
+        }
+        return births;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String test = "null";
+        String toTest = "Something";
+        System.out.println(test.length());
+        System.out.println(toTest.contains(test));
+    }
+}
